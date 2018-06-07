15bf938c27c70884401762042de6d45cd9654b8a
AGMETEOR <guwats10@gmail.com>
2018-06-07 20:14:58 +0300

    Edit test file

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,8 +19,8 @@ class TestClass(unittest.TestCase):
         }
 
         with self.app.test_client() as c:
-            resss = c.post('auth/signup/',headers={'Content-Type': 'application/json; charset=utf-8'},data=json.dumps({"username": "testing me", "password": "iamsecret","status":"normal"}))
-            self.tk_res = c.post('auth/login/', headers={'Content-Type': 'application/json; charset=utf-8'}, data=json.dumps({"username": "testing me", "password": "iamsecret"}))
+            c.post('auth/signup/',headers={'Content-Type': 'application/json; charset=utf-8'},data=json.dumps({"username": "testingme", "password": "iamsecret","status":"normal"}))
+            self.tk_res = c.post('auth/login/', headers={'Content-Type': 'application/json; charset=utf-8'}, data=json.dumps({"username": "testingme", "password": "iamsecret"}))
             self.data = json.loads(self.tk_res.data.decode())
             self.res = c.post('api/v1/users/requests/', headers={'Authorization': self.data["token"],
                                                                  'Content-Type': 'application/json; charset=utf-8'}, data=json.dumps(self.request_body))
