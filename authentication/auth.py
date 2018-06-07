3002926e64a899786b8377211989ad7535af9f7e
AGMETEOR <guwats10@gmail.com>
2018-06-07 20:08:04 +0300

    Edit test file

--- authentication/auth.py
+++ authentication/auth.py
@@ -9,7 +9,7 @@ from functools import wraps
 from dbHandler import UserDatabaseHandler
 
 userdb = UserDatabaseHandler("test_db")
-# userdb.create_table("new_users_db")
+userdb.create_table("new_users_db")
 
 
 class AuthAPI(MethodView):
@@ -59,7 +59,7 @@ class AuthAPI(MethodView):
 
         else:
             return jsonify({"error": "Please provide username or password"}), 401
-            
+
     @staticmethod
     def login_required(f):
         @wraps(f)
