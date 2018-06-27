e9e5f62bb26a545c9ac30dca7b6cea0d875ce680
AGMETEOR <guwats10@gmail.com>
2018-06-27 18:43:34 +0300

    Add CORS

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 def create_app(mode):
     app = Flask(__name__)
 
-    CORS(app,CORS(app, resources=r'https://mt-trackr.herokuapp.com/*')
+    CORS(app, resources=r'/*')
     
 
     app.config.from_pyfile('settings.py')
